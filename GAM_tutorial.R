190ad984c3d073e3e086154d2e84bb493b1eec8b
Emily <lekroft@hotmail.com>
2020-07-15 11:30:15 -0500

    Completed second GAM tutorial

--- GAM_tutorial.R
+++ GAM_tutorial.R
@@ -47,3 +47,61 @@ plot(mod_city2, all.terms = TRUE, pages = 6)
 mod_city3 <- gam(city.mpg ~ s(weight, by = drive) + s(length, by = drive) + s(price, by = drive) + drive,
                  data = mpg, method = "REML")
 plot(mod_city3, pages = 6)
+
+##Part 2#############################################
+library(gamair)
+data("mpg", package="gamair")
+
+library(mgcv)
+
+mod_city4 <- gam(city.mpg ~ s(weight) + s(length) + s(price) + s(rpm) + s(width),
+                 data = mpg, method = "REML")
+summary(mod_city4)
+
+mod <- gam(accel ~ s(times), data = mcycle, method = "REML")
+plot(mod, residuals=TRUE)
+plot(mod, residuals=TRUE, pch=1, cex=1)
+mod <- gam(hw.mpg ~ s(weight) + s(rpm) + s(price) + comp.ratio, 
+           data = mpg, method = "REML")
+plot(mod, select=3)
+plot(mod, pages = 5, all.terms = TRUE)
+
+
+mod <- gam(hw.mpg ~ s(weight) + s(rpm) + s(price) + comp.ratio, 
+           data = mpg, method = "REML")
+plot(mod, select=1, shade=TRUE, shade.col="hotpink", pages=3)
+
+# Make another plot adding the intercept value and uncertainty
+plot(mod, select = 1, shade = TRUE, shade.col = "hotpink", 
+     shift = coef(mod)[1], seWithMean = TRUE, pages=3)
+
+set.seed(0)
+dat <- gamSim(1,n=200)
+
+mod <- gam(y ~ s(x0, k = 5) + s(x1, k = 5) + s(x2, k = 5) + s(x3, k = 5),
+           data = dat, method = "REML")
+gam.check(mod)
+
+set.seed(0)
+dat <- mgcv::gamSim(1,n=600, scale=0.6, verbose=FALSE)
+
+mod <- gam(y ~ s(x0, k = 3) + s(x1, k = 3) + s(x2, k = 3) + s(x3, k = 3),
+           data = dat, method = "REML")
+gam.check(mod)
+mod2 <- gam(y ~ s(x0, k =3) + s(x1, k = 3) + s(x2, k = 10) + s(x3, k =3),
+            data = dat, method = "REML")
+gam.check(mod2)
+
+set.seed(0)
+data("mpg", package="gamair", verbose=FALSE)
+mod <- gam(hw.mpg ~ s(length) + s(width) + s(height) + s(weight),
+           data = mpg, method = "REML")
+concurvity(mod, full = TRUE)
+
+set.seed(0)
+data("mpg", package="gamair", verbose=FALSE)
+
+mod <- gam(hw.mpg ~ s(length) + s(width) + s(height) + s(weight),
+           data = mpg, method = "REML")
+
+concurvity(mod, full=FALSE)
