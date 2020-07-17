5a8e90fa1d21c933f0aba7b6c7f8725ebdcabee1
Emily <lekroft@hotmail.com>
2020-07-17 10:37:17 -0500

    Tried applying GAM to some of my data

--- Testing_Models.R
+++ Testing_Models.R
@@ -5,6 +5,7 @@ View(data)
 LST.model<-lm(data$LST_mean~data$X.canopy+data$Imp.+data$BD+data$Income+data$river.distance..meters.)
 summary(LST.model)
 plot(LST.model)
+
 ##the diagnostic plots actually look good here, a linear model could work well
 
 ##############################NO2######################
@@ -23,6 +24,14 @@ lm.no2.ranked<-lm(NO2_ranked~BD+petrochem+road+income, data=ranked.data)
 summary(lm.no2.ranked)
 plot(lm.no2.ranked)
 
+NO2.gam<-gam(data$NO2_mean~s(data$BD)+s(data$petrochem_distance)+
+               s(data$Income), method="REML")
+summary(NO2.gam)
+coef(NO2.gam)
+plot(NO2.gam)
+
+plot(NO2.gam, residuals=TRUE, pch=1)
+
 ##rank transform helped but still not perfect
 
 ##########################GS#######################
@@ -33,3 +42,12 @@ plot(GS.model)
 ##sqrt transform seems to help
 ##log transform did not help
 ##rank transform did not help
+
+GS.gam<-gam(data$nearest_M~s(data$BD)+s(data$Income))
+summary(GS.gam)
+plot(GS.gam, residuals=TRUE, pch=1)
+
+
+GS.lm2<-lm(data$nearest_M~data$BD*data$Income)
+summary(GS.lm2)
+##still not great but better than first model
