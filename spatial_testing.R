d38a1c4feb434a760eead3bd04061c1e52697542
Emily <lekroft@hotmail.com>
2020-08-13 11:29:43 -0500

    cleaned up graphs

--- spatial_testing.R
+++ spatial_testing.R
@@ -1,20 +1,22 @@
 ##read in data and packages
 data<-read.csv("Data/Study_Site_coords.csv")
 library(gstat)
+library(sp)
 
 ######LST#####################
 
 #set up variogram
 breaks = seq(0, 1.5, l = 2)
+coordinates(data)= ~ Lat+Long
 
 bubble(data, zcol='LST_mean', fill=TRUE, do.sqrt=FALSE, maxsize=3)
 
 #plot variogram
 TheVariogram=variogram(LST_mean~1, data=data)
-plot(TheVariogram)
+plot(TheVariogram, pch=16, col="navy")
 
 #make variogram model
-TheVariogramModel <- vgm(psill=7, model="Exp", nugget=4.2, range=10000)
+TheVariogramModel <- fit.variogram(TheVariogram, vgm("Exp"))
 plot(TheVariogram, model=TheVariogramModel) 
 
 ####NO2###################
@@ -26,10 +28,10 @@ bubble(data, zcol='NO2_mean', fill=TRUE, do.sqrt=FALSE, maxsize=3)
 
 #plot variogram
 TheVariogram=variogram(NO2_mean~1, data=data)
-plot(TheVariogram)
+plot(TheVariogram, pch=16, col="forestgreen")
 TheVariogram
 
 #make variogram model
-TheVariogramModel <- vgm(psill=3e-11, model="Exp",
-                         nugget=5e-12, range=15000)
+TheVariogramModel <- fit.variogram(TheVariogram, vgm("Exp"))
 plot(TheVariogram, model=TheVariogramModel)
+
