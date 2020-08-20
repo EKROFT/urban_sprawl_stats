4017007db0f0c3fc76a4cbc3286d4f67e52150cf
Emily <lekroft@hotmail.com>
2020-08-20 12:14:27 -0500

    Restarted NO2 data exploration

--- LST_second_exploration.R
+++ LST_second_exploration.R
@@ -13,25 +13,43 @@ GGally::ggpairs(data.selected)
 #First attempt model
 lst.model<-lm(LST_mean~BD+X.canopy+Income+river.distance..meters.+Imp., data=lst.data)
 summary(lst.model)
+library(car)
+vif(lst.model)
 
-#canopy cover and impervious seem quite correlated, same with BD and impervious
+#canopy cover and impervious seem a little correlated, same with BD and impervious
 #I'm testing a model that removes impervious cover for that reason
 lst.model2<-lm(LST_mean~BD+X.canopy+Income+river.distance..meters., data=lst.data)
 summary(lst.model2)
+vif(lst.model2)
 
 #I want to test if the simpler model was better
 anova(lst.model,lst.model2)
-#looks like last model is better
+#looks like more complex model is better
+
+#since the vif showed nothing above 5 for first model, I will keep impervious
 
 ##checking model assumptions
-plot(lst.model2, which=2)
-plot(lst.model2, which=3)
+plot(lst.model, which=2)
+plot(lst.model, which=3)
+
+##now testing models with interactions
+
+lst.model5<-lm(LST_mean~BD+(X.canopy*Imp.)+Income+river.distance..meters., data=lst.data)
+summary(lst.model5)
+#canopy:imp. interaction has p values close to 1
+
+lst.model6<-lm(LST_mean~(BD*X.canopy)+Imp.+Income+river.distance..meters., data=lst.data)
+summary(lst.model6)
+#this BD-canopy cover interaction has a very high p value
+anova(lst.model, lst.model6)
+#model without interaction term is stronger
+
 
 library(nlme)
 library(gstat)
 library(sp)
 
-lst.model4<-lm(LST_mean~BD+X.canopy+river.distance..meters., data=lst.data)
+lst.model4<-lm(LST_mean~BD+X.canopy+river.distance..meters.+Imp., data=lst.data)
 coordinates(lst.data)<-c('Long','Lat')
 resids<-residuals(lst.model4)
 lst.data$resids=resids
